5c748693c51a4ae6bbb4f734ca08ff57f588c066
Wendao <xiewendao123@foxmail.com>
2022-12-19 22:30:32 +0800

    [update&fix] Dashboard settings & fix actions error

---
+++ scripts/dashboard.js
@@ -0,0 +1,52 @@
+function dataSource() {
+	return {
+		chart_test_1: [
+			{
+				value: 40300,
+				date: '2022-1-1'
+			},
+			{
+				value: 50800,
+				date: '2022-2-1'
+			},
+			{
+				value: 31300,
+				date: '2022-3-1'
+			},
+			{
+				value: 48800,
+				date: '2022-4-1'
+			},
+			{
+				value: 69900,
+				date: '2022-5-1'
+			},
+			{
+				value: 37800,
+				date: '2022-6-1'
+			}
+		],
+		chart_test_2: [
+			{
+				name: '毛毛',
+				stay: 3,
+				cost: 2000
+			},
+			{
+				name: '阿布',
+				stay: 6,
+				cost: 4200
+			},
+			{
+				name: '咪咪',
+				stay: 7,
+				cost: 6000
+			},
+			{
+				name: '狗蛋',
+				stay: 1,
+				cost: 1000
+			}
+		]
+	}
+}
