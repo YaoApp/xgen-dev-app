384f4a8e4d5f86e50cadbf6f78a37f852b8e6d74
Wendao <xiewendao123@foxmail.com>
2022-12-20 17:35:20 +0800

    [add] Dashboard Table support onChangeEvent?:boolean

--- scripts/event.js
+++ scripts/event.js
@@ -2,7 +2,9 @@ function OnChange(info) {
 	console.log(info)
 
 	let data = { attack: 10 } // 更新消费金额数据, 消费金额
-	let setting = Process('yao.form.Setting', 'hero', { name: 'Hero' + data.attack }) // 根据新数值生成配置信息
+      let setting = Process('yao.form.Setting', 'hero') // 根据新数值生成配置信息
+      
+      // 如果setting获取报错，Throw Error，让接口报错
 
 	return {
 		data: data, // 更新消费金额数据, 消费金额
