ada4d33d3a44b04e7146bbd1972db90da2ba287e
Wendao <xiewendao123@foxmail.com>
2023-04-19 08:30:20 +0800

    fixed untracked files

--- scripts/neo.js
+++ scripts/neo.js
@@ -1,13 +1,66 @@
-function chat(args) {
-	const date = new Date()
+const MaxTokens = 2048
+
+const test_reply = ['kaksdkaskdkas']
+
+function chat(content, context, token) {
+	console.log(content, context, token)
+
+	let cfg = setting()
+	let url = `${cfg.host}/v1/chat/completions`
+	let paylad = { model: cfg.model, messages: [{ role: 'user', content }], user: 'yao' }
+
+	return stream(url, paylad, cfg.key, (data) => {
+		const real_data = data.replace('data: ', '')
+
+		if (data.indexOf('[DONE]') !== -1) {
+			ssEvent('message', {
+				actions: [],
+				confirm: true,
+				done: true
+			})
+
+			return 0
+		}
+
+		if (real_data.indexOf(`"delta":{"content"`) !== -1) {
+			const formated_data = JSON.parse(real_data)
+			const text = formated_data.choices[0].delta.content
+
+			ssEvent('message', {
+				text,
+				done: false
+			})
+		}
+
+		return 1
+	})
+}
+
+function stream(url, payload, key, cb) {
+	payload['stream'] = true
+	let response = http.Stream('POST', url, cb, payload, null, {
+		Authorization: `Bearer ${key}`,
+		'Content-Type': 'application/json; charset=utf-8'
+	})
+
+	if (response.code != 200) {
+		let data = response.data || {}
+		let err = data.error || {}
+		let message = err.message || 'unknown error'
+		log.Error(`OpenAI API error ${message}`)
+		throw new Exception(`OpenAI API error ${message}`, response.code || 500)
+	}
+
+	return response.data
+}
+
+function setting() {
+	let vars = Process('utils.env.GetMany', 'OPENAI_MODEL', 'OPENAI_KEY', 'OPENAI_PROXY', 'OPENAI_MODEL_EMBEDDING')
 
 	return {
-		text: `<Test>Hello, World!</Test>
-
-            现在的时间是：
-            ${date.getFullYear()}-${
-			date.getMonth() + 1
-		}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}
-            `
+		model: vars['OPENAI_MODEL'] || 'gpt-3.5-turbo',
+		model_embedding: vars['OPENAI_MODEL_EMBEDDING'] || 'text-embedding-ada-002',
+		key: vars['OPENAI_KEY'],
+		host: vars['OPENAI_PROXY'] ? vars['OPENAI_PROXY'] : 'https://api.openai.com'
 	}
 }
---
+++ scripts/select.js
@@ -0,0 +1,21 @@
+function options() {
+	return [
+		{
+			value: 'jack',
+			label: 'Jack'
+		},
+		{
+			value: 'lucy',
+			label: 'Lucy'
+		},
+		{
+			value: 'disabled',
+			disabled: true,
+			label: 'Disabled'
+		},
+		{
+			value: 'Yiminghe',
+			label: 'yiminghe'
+		}
+	]
+}
